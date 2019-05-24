f9abc170ca7374e5538a54b5034a4fa43ce777b0
Delagardi <delagardi@ymail.com>
2019-05-24 03:14:00 +0300

    now you can Add education to the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,7 +211,6 @@ route.put('/experience', [auth,
 
 // @route api/profile/experience/:exp_id
 // @desc Deleting experience item from profile by ID
-
 route.delete(
   '/experience/:exp_id', 
   auth, 
@@ -235,6 +234,70 @@ route.delete(
       console.error(error.message);
       res.status(500).send('Server error');
     }
-  })
+  }
+);
+
+// @route api/profile/education
+// @desc Updating education in profile
+route.put(
+  '/education', 
+  [
+    auth,
+    [
+      check('school', 'School is required')
+        .not()
+        .isEmpty(),
+      check('degree', 'Degree is required')
+        .not()
+        .isEmpty(),
+      check('fieldofstudy', "Field of study is required")
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newEducation = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEducation);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = route;
