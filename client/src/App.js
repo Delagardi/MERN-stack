02bd97a2a7cb38503f01e9675b5ecf55f774d33f
Delagardi <delagardi@ymail.com>
2019-06-02 00:02:07 +0300

    Links are working now with React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/navbar';
 import Landing from './components/layout/landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/login';
+import Registration from './components/auth/registration';
 
-import './App.css';
+import './app.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component={Landing}/>
+        <div className="container">
+        <Switch>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/registration" component={Registration}/>
+        </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
