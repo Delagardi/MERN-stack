60c4e0b1a8bf23b99e1b9a31722943120e3e0034
Delagardi <delagardi@ymail.com>
2019-06-02 00:43:58 +0300

    Added login form

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (event) => setFormData({ ...formData, [event.target.name]: event.target.value});
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log(formData);    
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Login to Your Account</p>
+      <form 
+        className="form" 
+        action="login-profile.html"
+        onSubmit={ (event) => onSubmit(event)}>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email Address" 
+            name="email"
+            value={email}
+            onChange={ (event) => onChange(event) }
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={ (event) => onChange(event) }
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/registration">Sign Up</Link>
+      </p>
+    </Fragment>
   )
 }
 
-export default Login
+export default Login;
--- client/src/components/auth/registration.js
+++ client/src/components/auth/registration.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
   const [formData, setFormData] = useState({
@@ -79,7 +80,7 @@ const Registration = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   )
